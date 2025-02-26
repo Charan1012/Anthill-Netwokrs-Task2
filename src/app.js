428999ea8ce26d86d0983b51c7061a6fe8cad625
Charan1012 <charan.r2022eee@sece.ac.in>
2025-02-27 00:52:08 +0530

    Task completed

--- src/app.js
+++ src/app.js
@@ -43,6 +43,9 @@ app.use('/api/bookings', bookingRoutes);
 
 app.use(errorHandler);
 
-
+app.get('/', (req, res) => {
+    res.send('Welcome to the Bus Booking System API!');
+});
+  
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
